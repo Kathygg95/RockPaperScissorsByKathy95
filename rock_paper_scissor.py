a8577c39c2bccd48862aa2afa3b4a8d22669d3d5
Kathy <katgg95@gmail.com>
2023-05-27 22:17:57 +0300

    Add scores functionality

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -21,25 +21,38 @@ def check_guess(computer_choice, guess):
         (computer_choice == 's' and guess == 'p'):
         print(Fore.RED + 'You lose!')
         print(Style.RESET_ALL)
+        return -1
+        
     elif (guess == 'r' and computer_choice == 's') or\
         (guess == 'p' and computer_choice == 'r')or\
         (guess == 's' and computer_choice == 'p'):
         print(Fore.GREEN + 'You win!')
         print(Style.RESET_ALL)
+        return 1
     elif (guess == 'r' and computer_choice == 'r') or\
         (guess == 'p' and computer_choice == 'p')or\
         (guess == 's' and computer_choice == 's'):
         print(Fore.YELLOW +'Draw!')
         print(Style.RESET_ALL)
-          
-while True:
-    result_computer = choice_list(['r', 'p', 's'])
-    theguess = user_guess()
-    print(f'The computer chose {result_computer}')
-    check_guess(result_computer, theguess)
-    replay = input('Continue (yes or no): ')
-    if replay == 'yes':
-        continue 
-    else:
-        break      
-       
+        return 0
+
+def game():
+    count_computer = 0
+    count_player = 0
+            
+    while True:
+        result_computer = choice_list(['r', 'p', 's'])
+        theguess = user_guess()
+        print(f'The computer chose {result_computer}')
+        result = check_guess(result_computer, theguess)
+        if result == -1:
+            count_computer += 1
+        if result == 1:
+            count_player += 1
+        replay = input('Continue (yes or no): ')
+        if replay == 'yes':
+            continue 
+        else:
+            print(f'Computer score: {count_computer} \nPlayer score: {count_player}')
+            break      
+game()      
